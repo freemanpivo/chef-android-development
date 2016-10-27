c2fec8709de9adc9fbf5286de6f1e3ddce27a633
Pedro Ivo Andrade <andradepedroivo@gmail.com>
2016-10-27 03:02:00 -0200

    Adding android studio folder and download setup

--- cookbook/android-studio/recipes/default.rb
+++ cookbook/android-studio/recipes/default.rb
@@ -25,9 +25,35 @@ directory '/home/android' do
 	not_if {File.exist?("/home/android/")}
 end
 
-#Install Dependencies for amd64 OS.
-execute "installing libs for amd64 architecture"
+# Install Dependencies for amd64 OS.
+execute "installing libs for amd64 architecture" do
 	command "apt-get install -y libtinfo5"
 	user "root"
 	action :run
+end
+
+# List of packages needed
+package 'wget'
+package 'unzip'
+
+# Create studio folder in android user.
+directory '/home/android/android-studio' do
+    owner 'android'
+    mode '0755'
+    action :create
+end
+
+# Android Studio Settings
+android_studio_version = "2.2.1.0"
+android_studio_build = "145.3330264"
+android_studio_file = android_studio_version + '/android-studio-ide-' + android_studio_build + '-linux.zip'
+android_studio_dlpath = "https://dl.google.com/dl/android/studio/ide-zips/"
+android_studio_link = android_studio_dlpath + android_studio_file
+
+# Install Android Studio 
+remote_file '/home/android/android-studio/android-studio.zip' do
+    source android_studio_link
+    owner 'android'
+    mode '0755'
+    action :create
 end
