afd6cd8402e5091170d3a3c4412592bb897d45f1
Pedro Ivo Andrade <andradepedroivo@gmail.com>
2016-10-31 14:59:07 -0200

    Creating virtualBox installation script

--- cookbook/virtualbox/recipes/default.rb
+++ cookbook/virtualbox/recipes/default.rb
@@ -6,4 +6,28 @@
 #
 # 
 
+virtual_box_link = "http://download.virtualbox.org/virtualbox/5.1.6/VirtualBox-5.1.6-110634-Linux_amd64.run"
+
+# Create VirtualBox installation folder
+directory '/home/android/virtualbox' do
+    owner 'android'
+    mode '0755'
+    action :create
+end
+
+# Download VirtualBox
+remote_file '/home/android/virtualbox/virtualbox.run' do
+    source virtual_box_link
+    owner 'android'
+    mode '0755'
+    action :create
+end
+
+# Install VirtualBox
+execute "Installing Virtual Box" do
+    command "sudo chmod +x virtualbox.run"
+    command "sudo ./virtualbox.run"
+    user "android"
+    action :run
+end
 
