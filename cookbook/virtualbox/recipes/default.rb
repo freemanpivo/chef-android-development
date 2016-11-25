9ffa0e10f450a1cc0f03d097851c5bc600122911
Pedro Ivo Andrade <andradepedroivo@gmail.com>
2016-11-25 05:16:47 -0200

    Updating virtualbox recipe

--- cookbook/virtualbox/recipes/default.rb
+++ cookbook/virtualbox/recipes/default.rb
@@ -6,28 +6,15 @@
 #
 # 
 
-virtual_box_link = "http://download.virtualbox.org/virtualbox/5.1.6/VirtualBox-5.1.6-110634-Linux_amd64.run"
-
-# Create VirtualBox installation folder
-directory '/home/android/virtualbox' do
-    owner 'android'
-    mode '0755'
-    action :create
+execute "install virtualbox" do
+    command "apt-get install -y virtualbox"
+    user "root"
+    action :run
 end
 
-# Download VirtualBox
-remote_file '/home/android/virtualbox/virtualbox.run' do
-    source virtual_box_link
-    owner 'android'
-    mode '0755'
-    action :create
-end
-
-# Install VirtualBox
-execute "Installing Virtual Box" do
-    command "sudo chmod +x virtualbox.run"
-    command "sudo ./virtualbox.run"
-    user "android"
+execute "install virtualbox-dkms" do
+    command "apt-get install -y virtualbox-dkms"
+    user "root"
     action :run
 end
 
